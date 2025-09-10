8282ab8353151282ec0ddae29b11dd403493d962
juanc <juanc@MSI>
2025-09-10 00:17:37 -0500

    asdfadsfasdfasdfadsfAgregar archivos de proyecto.

---
+++ Backend/Services/Library/Library.Service/Framework/Web/EndpointExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Library.Service.Framework.Web
+{
+    public static class EndpointExtensions
+    {
+        public static IEndpointRouteBuilder MapAllEndpoints(this IEndpointRouteBuilder app)
+        {
+            Rest.Endpoints.PersonsEndpoints.MapPersonsEndpoints(app);
+            return app;
+        }
+    }
+}
---
+++ Backend/Services/Library/Library.Service/Program.cs
@@ -0,0 +1,12 @@
+
+using Library.Service.Framework;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHostServices(builder.Configuration);
+
+var app = builder.Build();
+
+await app.UseHostSetupAsync();
+
+app.Run();
